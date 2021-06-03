f7f6cbbfb9ab2c7f342d8841cb1bda47a367571d
Azarov Oleg <74871854+deels@users.noreply.github.com>
2021-06-03 17:48:28 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,11 @@
+# Task 1
+# A simple function.
+# Create a simple function called favorite_movie, which takes a string containing the name
+# of your favorite movie. The function should then print “My favorite movie is named {name}”.
+
+def favourite_movie():
+    movie = input('Введите название любимого фильма: ')
+    print(f'Мой любимый фильм называется {movie}.')
+    return
+
+favourite_movie()
